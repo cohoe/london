d15d433c43c95d5179e93bb53ddd788f5ba92769
Grant Cohoe <grant@grantcohoe.com>
2021-02-20 17:54:28 -0500

    improve error handling

--- london/commands/recipe.py
+++ london/commands/recipe.py
@@ -4,5 +4,3 @@ from london.commands.resource import ResourceCommand
 class Recipe(ResourceCommand):
     endpoint = 'http://localhost:8080/api/v1/cocktails'
     path = './recipes'
-
-    # @TODO 2 are missing from the 221
--- london/resource/__init__.py
+++ london/resource/__init__.py
@@ -1,41 +1,55 @@
 import london.util
 import requests
+from barbados.services.logging import LogService
 
 
 class Resource:
     @staticmethod
     def get(endpoint, args):
+        LogService.info("Getting %s" % args.slug)
         if args.slug == 'all':
             result = requests.get(endpoint)
         else:
             result = requests.get("%s/%s" % (endpoint, args.slug))
-        print(result.json())
-        return Resource._handle_error(result)
+        # print(result.json())
+        Resource._handle_error(result)
 
     @staticmethod
     def create(endpoint, args, path):
+        LogService.info("Creating %s" % args.slug)
         data = london.util.load_yaml_data_from_path(path)
+        LogService.info("Found %i items." % len(data))
+        success = 0
         for item in data:
             if item.get('slug') == args.slug or args.slug == 'all':
-                print(item)
+                # print(item)
                 result = requests.post(endpoint, json=london.util.to_json(item))
-
-        return Resource._handle_error(result)
+                success += Resource._handle_error(result)
+        LogService.info("Succeeded with %i items." % success)
 
     @staticmethod
     def delete(endpoint, args):
+        LogService.info("Deleting %s" % args.slug)
         if args.slug == 'all':
             result = requests.delete(endpoint)
         else:
             result = requests.delete("%s/%s" % (endpoint, args.slug))
-        return Resource._handle_error(result)
+
+        Resource._handle_error(result)
 
     @staticmethod
     def _handle_error(result):
+        """
+        Parse an HTTP request for success/failure. Return a count of the success.
+        :param result:
+        :return: Integer count of success (THIS IS NOT A RETURN CODE!)
+        """
         try:
             result.raise_for_status()
-            print("Success!")
+            LogService.debug('Success!')
+            return 1
         except requests.exceptions.RequestException as e:
-            print(result.text)
-            print(result.status_code)
-        return result
+            LogService.error("Error handling URL: %i" % result.status_code)
+            LogService.error(result.request.body)
+            LogService.error(result.text)
+            return 0
